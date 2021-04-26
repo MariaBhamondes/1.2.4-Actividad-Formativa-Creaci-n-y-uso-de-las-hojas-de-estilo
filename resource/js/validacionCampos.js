2c30a712d17dda5f13297be9396931f95841ba77
FernandaRomeroC <mf.romero.cancino@gmail.com>
2021-04-26 19:14:35 -0400

    Formulario funcionando correctamente

--- resource/js/validacionCampos.js
+++ resource/js/validacionCampos.js
@@ -22,14 +22,11 @@ let id_campos = ["nombre", "email", "documento", "ciudad", "comentario"];
 
 //Funcion botón enviar
 function enviarDatos(e){
-    //guardar campos que no tienen datos
-    let campo_no_valido = [];
     var contador = 0;
 
     //agregar a la lista campo_no_valido los input que no tienen datos
     for (let i = 0; i < id_campos.length; i++) {
         if(!validaCampo(id_campos[i])){
-            campo_no_valido.push(id_campos[i]);
             contador += 1;
         }
     }
@@ -41,15 +38,6 @@ function enviarDatos(e){
     }
     else{
         e.preventDefault();
-        document.getElementById("message").innerHTML="LOS SIGUIENTES <b>"+ contador + "</b> CAMPOS ESTÁN <b>VACÍOS </b> <hr>";
-        var listado;
-        for (let i = 0; i < campo_no_valido.length; i++) {
-            var li = document.createElement('li');
-            var p = document.createElement('p');
-            listado = "- "+campo_no_valido[i].toUpperCase();
-            p.append(document.createTextNode(listado));
-            document.querySelector("#message").appendChild(li).appendChild(p);
-        }
     } 
 }
 
